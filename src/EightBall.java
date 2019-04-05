30562a8cc955133654581f4dd4759612af6e4696
TKopp <TKopp@ZZTKopp01.microfocus.com>
2019-04-05 17:23:39 +0200

    Here was another change for Defect #24003

--- src/EightBall.java
+++ src/EightBall.java
@@ -11,7 +11,7 @@ public class EightBall {
 		}
 		new FileReader(filename).read(buffer);
 		System.out.println(buffer);
-		System.out.println(buffer); //Another useless line
+//removed a line here
 		System.out.println(buffer); //Here was a change for Defect #24003 
 	}
 }
