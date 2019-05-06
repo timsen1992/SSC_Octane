c53909e80f8c8df23e2565bdb61f506259ba24e6
timsen1992 <48806852+timsen1992@users.noreply.github.com>
2019-05-06 09:28:46 +0200

    Update EightBall.java

--- src/EightBall.java
+++ src/EightBall.java
@@ -11,7 +11,5 @@ public class EightBall {
 		}
 		new FileReader(filename).read(buffer);
 		System.out.println(buffer);
-		System.out.println(buffer);//added
-		System.out.println(buffer); //Here was a change for Defect #24003 
 	}
 }
