f698f0bc1e6cec51d29779000bdf9eb499fbc766
timsen1992 <48806852+timsen1992@users.noreply.github.com>
2019-11-08 20:32:42 +0100

    Update EightBall.java

--- src/my/Main/EightBall.java
+++ src/my/Main/EightBall.java
@@ -27,5 +27,9 @@ public class EightBall {
 		System.out.println(buffer);
 		System.out.println(buffer);
 		System.out.println(buffer);
+		
+		System.out.println(buffer);
+		System.out.println(buffer);
+		System.out.println(buffer);
 	}
 }
