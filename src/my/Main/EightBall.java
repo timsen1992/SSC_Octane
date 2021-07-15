b5630fbaa9f2f0d338f751fec3dcb1e0eb797e9a
timsen1992 <48806852+timsen1992@users.noreply.github.com>
2021-07-15 08:47:42 +0200

    Update EightBall.java

--- src/my/Main/EightBall.java
+++ src/my/Main/EightBall.java
@@ -20,25 +20,25 @@ public class EightBall {
 		int out = fr.read(buffer);
 		System.out.println(buffer);
 		
-		FileReader fra = new FileReader(filename);
-		int outa = fra.read(buffer);
-		System.out.println(buffer);
+		//FileReader fra = new FileReader(filename);
+		//int outa = fra.read(buffer);
+		//System.out.println(buffer);
 		
-		FileReader frb = new FileReader(filename);
-		int outb = frb.read(buffer);
-		System.out.println(buffer);
+		//FileReader frb = new FileReader(filename);
+		//int outb = frb.read(buffer);
+		//System.out.println(buffer);
 		
-		FileReader frc = new FileReader(filename);
-		int outc = frc.read(buffer);
-		System.out.println(buffer);
+		//FileReader frc = new FileReader(filename);
+		//int outc = frc.read(buffer);
+		//System.out.println(buffer);
 		
-		FileReader frd = new FileReader(filename);
-		int outd = frd.read(buffer);
-		System.out.println(buffer);
+		//FileReader frd = new FileReader(filename);
+		//int outd = frd.read(buffer);
+		//System.out.println(buffer);
 		
-		FileReader fre = new FileReader(filename);
-		int oute = fre.read(buffer);
-		System.out.println(buffer);
+		//FileReader fre = new FileReader(filename);
+		//int oute = fre.read(buffer);
+		//System.out.println(buffer);
 		
 		//FileReader frf = new FileReader(filename);
 		//int outf = frf.read(buffer);
